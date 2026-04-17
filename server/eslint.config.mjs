1187e0167301eac24fc004679fb50b5ff20e00b8
Brijesh <softbrijeshio@gmail.com>
2026-04-17 12:42:14 +0530

    feat: add ESLint import plugins, update module imports, and enhance config setup

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   {
@@ -12,6 +14,10 @@ export default tseslint.config(
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
   {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      import: importPlugin, // 👈 REGISTER PLUGIN
+    },
     languageOptions: {
       globals: {
         ...globals.node,
@@ -26,10 +32,35 @@ export default tseslint.config(
   },
   {
     rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+
+      // 👇 NOW THIS WILL WORK
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'type',
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
     },
   },
 );
